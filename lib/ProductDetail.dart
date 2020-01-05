e265a0b9a7d1be2dd520a179eb61fdf358ce09e0
baskoro aji susetyo <baskoroajisusetyo@gmail.com>
2020-01-05 16:42:20 +0700

    detail page

--- lib/ProductDetail.dart
+++ lib/ProductDetail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'models/Product.dart';
+import 'RatingBox.dart';
 
 class ProductDetail extends StatelessWidget{
   ProductDetail({Key key, this.item,}):super(key:key);
@@ -11,6 +12,32 @@ class ProductDetail extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text(this.item.name),),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset("assets/" + this.item.image),
+                Expanded(
+                    child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(this.item.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                            Text(this.item.description),
+                            Text("Price: " + this.item.price.toString()),
+                            RatingBox(),
+                          ],
+                        )
+                    )
+                )
+              ]
+          ),
+        ),
+      ),
     );
   }
 }
